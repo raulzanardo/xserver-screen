6ff59de692e19f74ab116c88a41350fb08361b28
raulzanardo <raulzanardo@gmail.com>
2025-07-26 18:31:43 -0300

    change

--- GridTransformer.h
+++ GridTransformer.h
@@ -1,65 +1,59 @@
-// LED matrix library transformer to map a rectangular canvas onto a complex
-// chain of matrices.
-// Author: Tony DiCola
+// GridTransformer.h
 #ifndef GRIDTRANSFORMER_H
 #define GRIDTRANSFORMER_H
 
 #include <cassert>
 #include <vector>
+#include "led-matrix.h" // Ensure this is included
 
-#include "led-matrix.h"
+namespace rgb_matrix {
+  class Canvas; // Forward declaration if needed
+  class CanvasTransformer; // Forward declaration if needed
+}
 
-
-class GridTransformer: public rgb_matrix::Canvas, public rgb_matrix::CanvasTransformer {
+class GridTransformer : public rgb_matrix::Canvas, public rgb_matrix::CanvasTransformer {
 public:
-  struct Panel {
-    int order;
-    int rotate;
-    int parallel;
-  };
+    struct Panel {
+        int order;
+        int rotate;
+        int parallel;
+    };
 
-  GridTransformer(int width, int height, int panel_width, int panel_height,
-                  int chain_length, const std::vector<Panel>& panels);
-  virtual ~GridTransformer() {}
+    GridTransformer(int width, int height, int panel_width, int panel_height,
+                    int chain_length, const std::vector<Panel>& panels);
 
-  // Canvas interface implementation:
-  virtual int width() const {
-    return _width;
-  }
-  virtual int height() const {
-    return _height;
-  }
-  virtual void Clear() {
-    assert(_source != NULL);
-    _source->Clear();
-  }
-  virtual void Fill(uint8_t red, uint8_t green, uint8_t blue) {
-    assert(_source != NULL);
-    _source->Fill(red, green, blue);
-  }
-  virtual void SetPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
+    virtual ~GridTransformer() {}
 
-  // Transformer interface implementation:
-  virtual rgb_matrix::Canvas* Transform(rgb_matrix::Canvas* source);
-
-  // Other attribute accessors.
-  int getRows() const {
-    return _rows;
-  }
-  int getColumns() const {
-    return _cols;
-  }
+    // Canvas interface implementation:
+    virtual int width() const {
+        return _width;
+    }
+    virtual int height() const {
+        return _height;
+    }
+    virtual void Clear() {
+        assert(_source != NULL);
+        _source->Clear();
+    }
+    virtual void Fill(uint8_t red, uint8_t green, uint8_t blue) {
+        assert(_source != NULL);
+        _source->Fill(red, green, blue);
+    }
+    virtual void SetPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
+    // Transformer interface implementation:
+    virtual rgb_matrix::Canvas* Transform(rgb_matrix::Canvas* source);
+    // Other attribute accessors.
+    int getRows() const {
+        return _rows;
+    }
+    int getColumns() const {
+        return _cols;
+    }
 
 private:
-  int _width,
-      _height,
-      _panel_width,
-      _panel_height,
-      _chain_length,
-      _rows,
-      _cols;
-  rgb_matrix::Canvas* _source;
-  std::vector<Panel> _panels;
+    int _width, _height, _panel_width, _panel_height, _chain_length, _rows, _cols;
+    rgb_matrix::Canvas* _source;
+    std::vector<Panel> _panels;
 };
 
-#endif
+#endif // GRIDTRANSFORMER_H
